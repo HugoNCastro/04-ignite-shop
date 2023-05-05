a5f96478e68987ac39d5a1a812165162ef84b21d
HugoNCastro <hugonaaman@gmail.com>
2023-05-05 16:05:30 -0300

    Adiciona Stitches e cria estilos globais

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 import { Roboto } from 'next/font/google'
 
@@ -7,6 +8,8 @@ const roboto = Roboto({
   subsets: ['latin'],
   display: 'swap',
 })
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
